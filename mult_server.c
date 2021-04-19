6969f370a663fd635f697cecef29ddf966ca1542
carlosrvilela <50680051+carlosrvilela@users.noreply.github.com>
2021-04-18 21:57:34 -0300

    Update mult_server.c

--- mult_server.c
+++ mult_server.c
@@ -1,44 +1,10 @@
 #include "mult.h"
-
-void
-mult_prog_1(char *host, int x, int y)
+int *
+mult_1_svc(numbers *argp, struct svc_req *rqstp)
 {
-CLIENT *clnt;
-int *result_1;
-numbers mult_1_arg;
-#ifndef DEBUG
-clnt = clnt_create (host, MULT_PROG, MULT_VERS, "udp");
-if (clnt == NULL) {
-clnt_pcreateerror (host);
-exit (1);
-}
+static int result;
 
-#endif /* DEBUG */
-mult_1_arg.a=x;
-mult_1_arg.b=y;
-result_1 = mult_1(&mult_1_arg, clnt);
-if (result_1 == (int *) NULL) {
-clnt_perror (clnt, "call failed");
-}
-else {
-printf("Resultado:%d\n", *result_1);
-}
-#ifndef DEBUG
-clnt_destroy (clnt);
-#endif /* DEBUG */
-}
-
-int
-main (int argc, char *argv[])
-{
-
-char *host;
-
-if (argc < 4) {
-printf ("usage: %s server_host numero1 numero2\n", argv[0]);
-exit (1);
-}
-host = argv[1];
-sub_prog_1 (host, atoi(argv[2]), atoi(argv[3]));
-exit (0);
+printf("Funcao mult(%d, %d) foi chamada\n", argp->a, argp->b);
+result = argp->a * argp->b;
+return &result;
 }
